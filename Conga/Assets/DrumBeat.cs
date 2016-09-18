3407f38916f020f615808d74a032078f04570694
Andrew Kennedy <akennedy2292@gmail.com>
2016-09-17 20:22:34 -0700

    add drum and sound asset

---
+++ Conga/Assets/DrumBeat.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrumBeat : MonoBehaviour {
+
+	public GameObject PlayerHands;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
