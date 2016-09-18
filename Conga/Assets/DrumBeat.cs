2c254350ce8bf9e8bc99bb4837b5a0d0420128ed
Andrew Kennedy <akennedy2292@gmail.com>
2016-09-17 21:42:40 -0700

    implement hand colliders and sound triggers. you can now play drums!

--- Conga/Assets/DrumBeat.cs
+++ Conga/Assets/DrumBeat.cs
@@ -1,17 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(AudioSource))]
 public class DrumBeat : MonoBehaviour {
 
-	public GameObject PlayerHands;
+	//public GameObject PlayerHands;
+	private AudioSource audio;
 
 	// Use this for initialization
 	void Start () {
-		
+		 audio = GetComponent<AudioSource>();
+
+		//AudioSource drumBeat = GetComponent<AudioSource> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+	}
+
+	void OnTriggerEnter (Collider other) {
+		Debug.Log("Hands collided with something");
+		if (other.gameObject.name == "Drum") {
+			Debug.Log ("You hit the drum");
+			audio.Play();
+		}
 	}
 }
